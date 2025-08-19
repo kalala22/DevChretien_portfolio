289abbed8412eaa9bcb4d0f88b6920bc0d87b67c
Chretien Kalala <chretienkalala22@gmail.com>
2025-08-19 22:11:11 +0100

    feat: création du composant NavbarView pour la navigation principale

---
+++ src/components/HomeView.vue
@@ -0,0 +1,29 @@
+<template>
+    <main class="min-h-screen bg-gray-100 flex flex-col md:flex-row items-center justify-between p-6">
+  
+  <!-- Texte -->
+  <div class="text-center md:text-left md:w-1/2 space-y-4">
+    <h1 class="text-4xl md:text-5xl font-medium"> 
+      Salut Je suis <span class="font-extrabold">Chretien Kalala,</span>
+    </h1>
+    <p class="text-2xl md:text-4xl font-extrabold">
+      Frontend Developer
+    </p>
+    <p class="text-base md:text-lg font-light">
+      Explorez mes projets et compétences pour en savoir plus sur mon travail.
+    </p>
+  </div>
+
+  <!-- Image -->
+  <div class="mt-8 md:mt-0 md:w-1/2 flex justify-center border border-red-500">
+    <div class="w-80 h-80 md:w-72 md:h-72 rounded-full overflow-hidden shadow-lg">
+      <img :src="myPic" alt="Ma photo" class="w-full h-full object-cover" />
+    </div>
+  </div>
+
+</main>
+</template>
+
+<script setup>
+import myPic from '@/assets/img/my_pic.jpeg'
+</script>
---
+++ src/components/NavbarView.vue
@@ -0,0 +1,31 @@
+
+<script setup>
+
+import { ref } from 'vue'
+
+const links = ref([
+  { name: 'Accueil', url: '#' },
+  { name: 'A propos', url: '#' },
+  { name: 'Projets', url: '#' },
+  { name: 'Compétences', url: '#' },
+  { name: 'Contact', url: '#' },
+])
+</script>
+
+<template>
+    <header class="flex items-center p-6 bg-gray-800 text-white sticky top-0 z-50 font-montserrat">
+    <div class="flex items-center justify-between w-full">
+      <!-- Logo à gauche -->
+      <div>
+        <a class="text-xl font-bold ">DevChretien</a>
+      </div>
+    
+      <!-- Liens à droite -->
+      <div class="flex items-center space-x-6">
+        <div v-for="link in links" :key="link.name" class="text-lg">
+          <a href="#">{{ link.name }}</a>
+        </div>
+      </div>
+    </div>
+  </header>
+</template>
