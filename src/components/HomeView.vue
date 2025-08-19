5e3188899344bd0a608c416a46f6376e8e1ab93e
Chretien Kalala <chretienkalala22@gmail.com>
2025-08-20 00:40:23 +0100

    feat:ajout des boutons CTA

--- src/components/HomeView.vue
+++ src/components/HomeView.vue
@@ -1,10 +1,10 @@
 <template>
-    <main class="min-h-screen bg-gray-100 flex flex-col md:flex-row items-center justify-between p-6">
+    <main class="min-h-screen bg-background flex flex-col md:flex-row items-center justify-between px-80">
   
   <!-- Texte -->
-  <div class="text-center md:text-left md:w-1/2 space-y-4">
-    <h1 class="text-4xl md:text-5xl font-medium"> 
-      Salut Je suis <span class="font-extrabold">Chretien Kalala,</span>
+  <div class="text-center  md:text-left md:w-1/2 space-y-4 text-primary ">
+    <h1 class="font-bold md:text-5xl"> 
+      Salut Je suis<br> <span class="font-extrabold text-7xl">Chretien Kalala,</span>
     </h1>
     <p class="text-2xl md:text-4xl font-extrabold">
       Frontend Developer
@@ -12,14 +12,20 @@
     <p class="text-base md:text-lg font-light">
       Explorez mes projets et compétences pour en savoir plus sur mon travail.
     </p>
+    <div>
+      <button class="bg-accent text-white px-6 py-3 rounded-lg hover:bg-red-600 transition-colors duration-300">
+        Contactez-moi
+      </button>
+      <button class="bg-primary text-secondary px-6 py-3 rounded-lg ml-4 hover:bg-blue-200 transition-colors duration-300">
+        Voir mon CV
+      </button>
+    </div>
   </div>
 
   <!-- Image -->
-  <div class="mt-8 md:mt-0 md:w-1/2 flex justify-center border border-red-500">
-    <div class="w-80 h-80 md:w-72 md:h-72 rounded-full overflow-hidden shadow-lg">
-      <img :src="myPic" alt="Ma photo" class="w-full h-full object-cover" />
+    <div class="md:w-72 md:h-72 border border-red-500">
+      <img :src="myPic" alt="Ma photo"/>
     </div>
-  </div>
 
 </main>
 </template>
