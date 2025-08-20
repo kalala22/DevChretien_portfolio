1a92aea897d7cfbcb2bf0080ed842dcd4d945afa
Chretien Kalala <chretienkalala22@gmail.com>
2025-08-20 02:07:16 +0100

    fix:erreur affichage sur mobile

--- src/components/HomeView.vue
+++ src/components/HomeView.vue
@@ -1,33 +1,38 @@
 <template>
-    <main class="min-h-screen bg-background flex flex-col md:flex-row items-center justify-between px-80">
+ 
+    <main class="min-h-screen bg-background flex items-center justify-center p-4">
   
-  <!-- Texte -->
-  <div class="text-center  md:text-left md:w-1/2 space-y-4 text-primary ">
-    <h1 class="font-bold md:text-5xl"> 
-      Salut Je suis<br> <span class="font-extrabold text-7xl">Chretien Kalala,</span>
-    </h1>
-    <p class="text-2xl md:text-4xl font-extrabold">
-      Frontend Developer
-    </p>
-    <p class="text-base md:text-lg font-light">
-      Explorez mes projets et compétences pour en savoir plus sur mon travail.
-    </p>
-    <div>
-      <button class="bg-accent text-white px-6 py-3 rounded-lg hover:bg-red-600 transition-colors duration-300">
-        Contactez-moi
-      </button>
-      <button class="bg-primary text-secondary px-6 py-3 rounded-lg ml-4 hover:bg-blue-200 transition-colors duration-300">
-        Voir mon CV
-      </button>
-    </div>
-  </div>
+        <div class="flex flex-col md:flex-row items-center justify-center gap-16 max-w-7xl">
+    
+            
+            <div class="text-center md:text-left space-y-4 text-primary order-2 md:order-1">
+                <h1 class="font-semibold text-4xl md:text-5xl"> 
+                    Salut Je suis<br> <span class="font-extrabold text-6xl md:text-7xl">Chretien Kalala,</span>
+                </h1>
+                <p class="text-2xl md:text-4xl font-extrabold">
+                    Full-Stack Developer
+                </p>
+                <p class="text-base md:text-lg font-light">
+                    Explorez mes projets et compétences pour en savoir plus sur mon travail.
+                </p>
+                <div class="flex flex-row sm:flex-row justify-center md:justify-start gap-4">
+                    <button class="bg-red-600 text-white px-6 py-3 rounded-lg hover:bg-accent transition-colors duration-300">
+                        Contactez-moi
+                    </button>
+                    <button class="bg-primary text-secondary px-6 py-3 rounded-lg hover:bg-blue-200 transition-colors duration-300">
+                        Voir mon CV
+                    </button>
+                </div>
+            </div>
 
-  <!-- Image -->
-    <div class="md:w-72 md:h-72 border border-red-500">
-      <img :src="myPic" alt="Ma photo"/>
-    </div>
+           
+            <div class="order-1 md:order-2 w-60 h-60 md:w-[30%] md:h-[30%] flex-shrink-0">
+                <img :src="myPic" alt="Ma photo" class="w-full h-full object-cover shadow-lg"/>
+            </div>
 
-</main>
+        </div>
+  
+    </main>
 </template>
 
 <script setup>
