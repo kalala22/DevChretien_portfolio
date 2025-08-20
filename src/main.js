292feae27872ea837c6dbd4188cbe953e4f3d458
Chretien Kalala <chretienkalala22@gmail.com>
2025-08-20 02:59:05 +0100

    fix:bug de l'affichage

--- src/main.js
+++ src/main.js
@@ -5,10 +5,10 @@ import App from './App.vue'
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 
-import { faUserSecret, faBars, faTimes, faEnvelope } from '@fortawesome/free-solid-svg-icons'
+import { faX, faBars, faTimes, faEnvelope } from '@fortawesome/free-solid-svg-icons'
 import { faGithub } from '@fortawesome/free-brands-svg-icons'
 
-library.add(faUserSecret, faBars, faTimes, faEnvelope, faGithub)
+library.add(faX, faBars, faTimes, faEnvelope, faGithub)
 
 // Enregistrez le composant FontAwesomeIcon globalement
 
