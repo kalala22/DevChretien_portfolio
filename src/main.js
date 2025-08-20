37667e629a8474a68b4a599a9efe29b157a5dc00
Chretien Kalala <chretienkalala22@gmail.com>
2025-08-20 10:16:40 +0100

    feat: intégration des icônes FontAwesome dans main.js

--- src/main.js
+++ src/main.js
@@ -5,10 +5,10 @@ import App from './App.vue'
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 
-import { faX, faBars, faTimes, faEnvelope } from '@fortawesome/free-solid-svg-icons'
+import { faX, faBars, faTimes, faEnvelope, faDownload } from '@fortawesome/free-solid-svg-icons'
 import { faGithub } from '@fortawesome/free-brands-svg-icons'
 
-library.add(faX, faBars, faTimes, faEnvelope, faGithub)
+library.add(faX, faBars, faTimes, faEnvelope, faGithub, faDownload)
 
 // Enregistrez le composant FontAwesomeIcon globalement
 
