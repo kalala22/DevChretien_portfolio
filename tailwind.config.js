44bd4aa0e76e383f8a8f5cfc1c98674276742e49
Chretien Kalala <chretienkalala22@gmail.com>
2025-08-20 03:58:29 +0100

    chore: mise à jour automatique du fichier package-lock.json

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,12 @@ export default {
         poppins: ['Poppins', 'sans-serif'],
         montserrat: ['Montserrat', 'sans-serif'],
       },
+      colors: {
+        primary: '#C2E6FF', // Light Blue
+        secondary: '#111927', // Blue Gray
+        accent: '#500F1C', // Red
+        background: '#0D1520', // Blue
+      },
     },
   },
 
