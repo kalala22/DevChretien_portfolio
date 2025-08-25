3e7c5c2d0a11bc22870f40f549ff4b06933d3d51
Chretien Kalala <chretienkalala22@gmail.com>
2025-08-25 20:33:42 +0100

    refactor: amélioration de la structure et du style du composant AboutSectionView

--- src/components/AboutSectionView.vue
+++ src/components/AboutSectionView.vue
@@ -1,11 +1,10 @@
 <template>
-  <div id="about" class="flex flex-col items-center justify-center bg-secondary p-5 h-screen mt-10">
-    <div class="absolute flex items-center justify-center opacity-10">
-      <font-awesome-icon icon="code" class="text-primary text-[15rem] md:text-[15rem]" />
-    </div>
-
+  <div id="about" class="flex flex-col items-center bg-base-300 justify-center p-5">
+    <!-- <div class="absolute flex items-center justify-center opacity-10">
+      <font-awesome-icon icon="code" class="text-primary/20 text-[15rem] md:text-[15rem]" />
+    </div> -->
     <div class="">
-      <h2 class="text-primary text-3xl md:text-5xl text-center uppercase">
+      <h2 class="neutral text-3xl md:text-5xl text-center uppercase">
         À propos de <span class="font-extrabold">Moi</span>
       </h2>
     </div>
@@ -17,16 +16,22 @@
         duration: 0.6,
         scale: { type: 'spring', visualDuration: 0.4, bounce: 0.5 },
       }"
+      class="md:flex md:items-center md:flex-row-reverse md:justify-center"
     >
-      <p class="text-primary text-lg md:text-2xl text-center mt-4 max-w-2xl">
-        <span class="underline decoration-decoration">Développeur web fullstack</span> passionné, je
+      <p class="neutral text-lg md:text-2xl text-center mt-4 max-w-2xl">
+        <span class="underline decoration-red-500">Développeur web fullstack</span> passionné, je
         conçois des applications modernes et interactives en alliant front-end, back-end et sens du
         design. Créatif et polyvalent, mon expérience en photographie et graphisme apporte une
         touche visuelle unique à mes projets.
       </p>
+      <div class="mt-10">
+        <img :src="flatImage" alt="image plat dev" class="md:h-96 drop-shadow-xl" />
+      </div>
     </motion.div>
   </div>
 </template>
 <script setup>
 import { motion } from 'motion-v'
+
+import flatImage from '@/assets/img/programmer_flat.png'
 </script>
