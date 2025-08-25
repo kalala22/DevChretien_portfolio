b8e10324ba59b22bae7f1dda8fec3648f0232173
Chretien Kalala <chretienkalala22@gmail.com>
2025-08-25 21:38:14 +0100

    refactor: amélioration de la structure du composant AboutSectionView en ajoutant un conteneur pour le texte

--- src/components/AboutSectionView.vue
+++ src/components/AboutSectionView.vue
@@ -3,7 +3,7 @@
     <!-- <div class="absolute flex items-center justify-center opacity-10">
       <font-awesome-icon icon="code" class="text-primary/20 text-[15rem] md:text-[15rem]" />
     </div> -->
-    <div class="">
+    <div class="mt-20">
       <h2 class="neutral text-3xl md:text-5xl text-center uppercase">
         À propos de <span class="font-extrabold">Moi</span>
       </h2>
@@ -18,12 +18,15 @@
       }"
       class="md:flex md:items-center md:flex-row-reverse md:justify-center"
     >
-      <p class="neutral text-lg md:text-2xl text-center mt-4 max-w-2xl">
-        <span class="underline decoration-red-500">Développeur web fullstack</span> passionné, je
-        conçois des applications modernes et interactives en alliant front-end, back-end et sens du
-        design. Créatif et polyvalent, mon expérience en photographie et graphisme apporte une
-        touche visuelle unique à mes projets.
-      </p>
+      <div class="">
+        <p class="neutral text-lg md:text-xl text-center mt-4 max-w-2xl">
+          <span class="underline decoration-red-500">Développeur web fullstack</span> passionné, je
+          conçois des applications modernes et interactives en alliant front-end, back-end et sens
+          du design. Créatif et polyvalent, mon expérience en photographie et graphisme apporte une
+          touche visuelle unique à mes projets.
+        </p>
+      </div>
+
       <div class="mt-10">
         <img :src="flatImage" alt="image plat dev" class="md:h-96 drop-shadow-xl" />
       </div>
