3069648200ea43678cb006c92d8aa1d17532742e
Chretien Kalala <chretienkalala22@gmail.com>
2025-08-25 20:30:11 +0100

    refactor: amélioration de la structure du composant CompetenceView et suppression de l'élément section inutile

--- src/components/CompetenceView.vue
+++ src/components/CompetenceView.vue
@@ -1,25 +1,27 @@
 <template>
-    <div>
-        <section id="competence" class="flex flex-col items-center justify-center bg-background p-5 py-20 mt-28">
-            <div class="absolute flex items-center justify-center opacity-10">
-                <font-awesome-icon icon="cogs" class="text-primary text-[15rem] md:text-[15rem]" />
-            </div>
-            <div>
-                <h2 class="text-primary text-3xl md:text-5xl text-center uppercase">Mes <span class="font-extrabold">Compétences</span></h2>
-            </div>
-            <div class="mt-8">
-                <p class="text-primary text-lg md:text-xl text-center max-w-2xl">
-                    En tant que développeur web fullstack, je maîtrise les technologies front-end et back-end pour créer des applications web modernes et performantes. Mon expertise couvre le développement d'interfaces utilisateur interactives, la gestion de bases de données et l'intégration d'API.
-                </p>
-            </div>
-
-        </section>
-        <section>
-            <Skils/>
-        </section>
-    </div>
+  <div class="">
+    <section id="competence" class="flex flex-col items-center justify-center p-5 py-20">
+      <div class="absolute flex items-center justify-center opacity-10">
+        <font-awesome-icon icon="cogs" class="text-primary text-[15rem] md:text-[15rem]" />
+      </div>
+      <div>
+        <h2 class="text-primary text-3xl md:text-5xl text-center uppercase">
+          Mes <span class="font-extrabold">Compétences</span>
+        </h2>
+      </div>
+      <div class="mt-8">
+        <p class="text-primary text-lg md:text-xl text-center max-w-2xl">
+          En tant que développeur web fullstack, je maîtrise les technologies front-end et back-end
+          pour créer des applications web modernes et performantes. Mon expertise couvre le
+          développement d'interfaces utilisateur interactives, la gestion de bases de données et
+          l'intégration d'API.
+        </p>
+      </div>
+    </section>
+    <Skils />
+  </div>
 </template>
 
 <script setup>
-    import Skils from './SkilsView.vue'
+import Skils from './SkilsView.vue'
 </script>
