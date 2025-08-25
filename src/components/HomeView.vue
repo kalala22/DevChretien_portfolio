aaaddfe9ce6f94fd7a94f661e173d688766d02a8
Chretien Kalala <chretienkalala22@gmail.com>
2025-08-26 00:14:51 +0100

    refactor: amélioration de la structure du composant HomeView en ajoutant un effet de frappe et en ajustant les styles

--- src/components/HomeView.vue
+++ src/components/HomeView.vue
@@ -2,6 +2,7 @@
 import myPic from '@/assets/img/my_pic.png'
 import BoutonCv from '@/components/BoutonCv.vue'
 import BoutonCallMe from '@/components/BoutonCallMe.vue'
+import TypingEffect from '@/components/TypingEffect.vue'
 import { ref, onMounted } from 'vue'
 
 const isLoaded = ref(false)
@@ -12,28 +13,26 @@ onMounted(() => {
 </script>
 
 <template>
-  <main id="home" class="min-h-screen flex items-center justify-center p-4">
+  <main id="home" class="flex items-center justify-center p-4">
     <transition name="fade-in">
-      <div
-        v-if="isLoaded"
-        class="flex flex-col md:flex-row items-center justify-center gap-16 max-w-7xl"
-      >
-        <div class="text-center md:text-left space-y-4 text-primary order-2 md:order-1">
-          <h1 class="font-semibold text-4xl md:text-5xl">
-            Salut Je suis<br />
-            <span class="font-extrabold text-6xl md:text-7xl">Chretien Kalala,</span>
+      <div v-if="isLoaded" class="flex flex-col md:flex-row items-center justify-between">
+        <div class="text-center md:text-left space-y-4 order-2 md:order-1">
+          <h1 class="font-normal neutral text-3xl md:text-4xl">
+            Salut, Je suis<br />
+            <span class="font-extrabold neutral text-4xl md:text-7xl">Chretien Kalala,</span>
           </h1>
-          <p class="text-2xl md:text-4xl font-extrabold">Full-Stack Developer</p>
-          <p class="text-base md:text-lg font-light">
+          <!-- <p class="text-2xl md:text-4xl font-extrabold neutral">Full-Stack Developer</p> -->
+          <TypingEffect class="neutral text-2xl md:text-3xl font-bold" />
+          <p class="md:text-lg text-xl font-normal neutral">
             Explorez mes projets et compétences pour en savoir plus sur mon travail.
           </p>
-          <div class="flex flex-row sm:flex-row justify-center md:justify-start gap-4">
+          <div class="flex flex-row sm:flex-row justify-center md:justify-start gap-4 mt-5">
             <BoutonCallMe />
             <BoutonCv />
           </div>
         </div>
 
-        <div class="order-1 md:order-2 w-60 h-60 md:w-[40%] md:h-[40%] flex-shrink-0">
+        <div class="order-1 md:order-2 w-80 h-80 md:w-[50%] md:h-[50%] flex-shrink-0">
           <img :src="myPic" alt="Ma photo" class="w-full h-full object-cover" />
         </div>
       </div>
