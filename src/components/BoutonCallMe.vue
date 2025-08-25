78117b9413ae6b5e522e87dafb1dea6996874b57
Chretien Kalala <chretienkalala22@gmail.com>
2025-08-25 22:12:17 +0100

    refactor: harmonisation des styles des boutons en unifiant les classes CSS et en ajoutant des styles de police

--- src/components/BoutonCallMe.vue
+++ src/components/BoutonCallMe.vue
@@ -7,7 +7,7 @@ const links = ref([{ name: 'Contact', href: '#contact' }])
 <template>
   <a
     :href="links[0].href"
-    class="bg-red-600 text-white px-2 py-3 rounded-lg hover:bg-accenttransition-colors duration-300 flex items-center gap-2"
+    class="bg-red-600 text-white font-bold px-2 py-3 rounded-lg hover:bg-red-700 duration-300 flex items-center"
   >
     Contactez-moi
     <font-awesome-icon icon="envelope" />
--- src/components/BoutonCv.vue
+++ src/components/BoutonCv.vue
@@ -1,17 +1,14 @@
 <template>
-   <a 
-    href="/CvFiles/CV_2025-08-20_Chretien_Kalala.pdf" 
-    target="_blank" 
+  <a
+    href="/CvFiles/CV_2025-08-20_Chretien_Kalala.pdf"
+    target="_blank"
     rel="noopener noreferrer"
-    class="bg-primary text-secondary px-6 py-3 rounded-lg hover:bg-blue-200 transition-colors duration-300  flex items-center gap-2"
+    class="bg-red-600 text-white font-bold px-6 py-3 rounded-lg hover:bg-red-700 transition-colors duration-300 flex items-center"
   >
-    Voir mon CV 
+    Voir mon CV
     <font-awesome-icon icon="download" />
   </a>
 </template>
-<script setup>
-
-  
-</script>
+<script setup></script>
 
 <!-- /assets/img/CvFiles/CV_2025-08-20_Chretien_Kalala -->
